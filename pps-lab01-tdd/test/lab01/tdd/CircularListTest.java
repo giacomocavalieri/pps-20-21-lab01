91bf16938d8e2001db876592a9386ee4a0a08604
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-01 12:34:08 +0100

    Add BeforeEach initialization in test

--- pps-lab01-tdd/test/lab01/tdd/CircularListTest.java
+++ pps-lab01-tdd/test/lab01/tdd/CircularListTest.java
@@ -1,4 +1,7 @@
+package lab01.tdd;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -6,12 +9,10 @@ import org.junit.jupiter.api.Test;
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    private CircularList list;
 
-    //TODO: test implementation
-
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    public void beforeEach() {
+        this.list = new CircularListImpl();
     }
-
 }
