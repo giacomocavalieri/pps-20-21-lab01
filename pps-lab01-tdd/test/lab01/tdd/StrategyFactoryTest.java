9ec1e166975eefa00db9013e638e8c34ef4bbd1e
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-03 16:45:31 +0100

    Refactor StrategyFactory tests using dynamic tests

--- pps-lab01-tdd/test/lab01/tdd/StrategyFactoryTest.java
+++ pps-lab01-tdd/test/lab01/tdd/StrategyFactoryTest.java
@@ -1,58 +1,52 @@
 package lab01.tdd;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StrategyFactoryTest {
     private final static StrategyFactory STRATEGY_FACTORY = new StrategyFactoryImpl();
-    private final static int TEST_LIST_SIZE = 50;
+    private final static int TEST_LIST_SIZE = 100;
     private final static List<Integer> TEST_LIST = IntStream.range(0, TEST_LIST_SIZE).boxed().collect(Collectors.toList());
 
-    @ParameterizedTest(name = "test{0}")
-    @MethodSource("provideStrategies")
-    public void testStrategy(final String strategyName, final SelectStrategy strategy,
-                             final List<Integer> matchingValues, final List<Integer> nonMatchingValues) {
-        testStrategyMatches(strategy, matchingValues);
-        testStrategyDoesNotMatch(strategy, nonMatchingValues);
-    }
-
-    private void testStrategyMatches(SelectStrategy strategy, Collection<Integer> matchingValues) {
-        assertTrue(matchingValues.stream().allMatch(strategy::apply));
-    }
-
-    private void testStrategyDoesNotMatch(SelectStrategy strategy, Collection<Integer> nonMatchingValues) {
-        assertTrue(nonMatchingValues.stream().noneMatch(strategy::apply));
-    }
-
-    private static Stream<Arguments> provideStrategies() {
+    @TestFactory
+    public List<DynamicTest> testStrategies() {
         final int testedMultiple = 3;
         final int testedEqual = TEST_LIST.get(0);
 
-        return Stream.of(
-            createArgumentFromPredicate("EvenStrategy", STRATEGY_FACTORY.evenStrategy(), n -> n % 2 == 0),
-            createArgumentFromPredicate("OddStrategy", STRATEGY_FACTORY.oddStrategy(), n -> n % 2 != 0),
-            createArgumentFromPredicate("MultipleOfStrategy", STRATEGY_FACTORY.multipleOfStrategy(testedMultiple), n -> n % testedMultiple == 0),
-            createArgumentFromPredicate("EqualStrategy", STRATEGY_FACTORY.equalStrategy(testedEqual), n -> n == testedEqual),
-            createArgumentFromPredicate("AllMatchingStrategy", STRATEGY_FACTORY.allMatchingStrategy(), n -> true),
-            createArgumentFromPredicate("NoneMatchingStrategy", STRATEGY_FACTORY.noneMatchingStrategy(), n -> false)
+        return List.of(
+            testExpectedBehaviour("testEvenStrategy", n -> n % 2 == 0, STRATEGY_FACTORY.evenStrategy()),
+            testExpectedBehaviour("testOddStrategy", n -> n % 2 != 0, STRATEGY_FACTORY.oddStrategy()),
+            testExpectedBehaviour("testMultipleOfStrategy", n -> n % testedMultiple == 0, STRATEGY_FACTORY.multipleOfStrategy(testedMultiple)),
+            testExpectedBehaviour("testEqualStrategy", n -> n == testedEqual, STRATEGY_FACTORY.equalStrategy(testedEqual)),
+            testExpectedBehaviour("testAllMatchingStrategy", n -> true, STRATEGY_FACTORY.allMatchingStrategy()),
+            testExpectedBehaviour("testNoneMatchingStrategy", n -> false, STRATEGY_FACTORY.noneMatchingStrategy())
         );
     }
 
-    private static Arguments createArgumentFromPredicate(final String testName, final SelectStrategy testedStrategy,
-                                                         final Predicate<Integer> matchingPredicate) {
-        final Map<Boolean, List<Integer>> listSplit = TEST_LIST.stream().collect(Collectors.partitioningBy(matchingPredicate));
-        return Arguments.of(testName, testedStrategy, listSplit.get(true), listSplit.get(false));
+    private DynamicTest testExpectedBehaviour(final String testName, final Predicate<Integer> expectedBehaviour,
+                                              final SelectStrategy testedStrategy) {
+        return DynamicTest.dynamicTest(testName, createTestRunnable(expectedBehaviour, testedStrategy));
     }
 
+    private Executable createTestRunnable(final Predicate<Integer> expectedBehaviour, final SelectStrategy testedStrategy) {
+        return () -> {
+            final Map<Boolean, List<Integer>> correctSplits = splitListWithPredicate(TEST_LIST, expectedBehaviour);
+            final Map<Boolean, List<Integer>> actualSplits = splitListWithPredicate(TEST_LIST, testedStrategy::apply);
+            assertEquals(correctSplits.get(true), actualSplits.get(true));
+            assertEquals(correctSplits.get(false), correctSplits.get(false));
+        };
+    }
+
+    private <T> Map<Boolean, List<T>> splitListWithPredicate(final List<T> list, final Predicate<T> splittingPredicate) {
+        return list.stream().collect(Collectors.partitioningBy(splittingPredicate));
+    }
 }
